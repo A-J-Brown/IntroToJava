5b161b2e4b3932e43a31a2fab24041d1bb75f85a
Alek Brown <alekjbrown@live.com>
2018-10-30 23:21:28 -0500

    remove cell bill, add Array List

--- src/ArrayListExampleBrown.java
+++ src/ArrayListExampleBrown.java
@@ -21,5 +21,64 @@ public class ArrayListExampleBrown
    {
        ArrayList<String> names = new ArrayList<String>();
        System.out.println(names);
+       
+     
+       //A.
+       names.add("Andy");
+       names.add("Angela");
+       names.add("Dwight");
+       names.add("Erin");
+       names.add("Jim");
+       names.add("Michael");
+       names.add("Pam");
+       names.add("Toby");
+       System.out.print("Entire array list: ");
+       System.out.println(names);
+       
+       
+       //B.
+       String output = "";
+       for(int i=0; i<names.size(); i++) {
+    	  output = output + names.get(i) + ", ";
+       }
+	   System.out.println("Output all names using get method: " + output);
+       
+       
+       //C
+       System.out.println( "Array Size: " + names.size());
+       
+       
+       //D
+       System.out.println("Last name in array: " + names.get(names.size()-1));
+       
+       
+       //E
+       names.set(0, "Andy Bernard");
+       System.out.println("Replaced Andy with Andy Bernard: " + names.get(0));
+       
+       
+       //F
+       names.add(5, "Kelly");
+       System.out.println("Add Kelly before Jim: " + names);
+       
+       
+       //G
+       System.out.print("Print names via enhanced for loop: ");
+       for(String element : names) {
+    	   System.out.print(element + ", ");
+       }
+       System.out.println();
+       
+       
+       //H
+       ArrayList<String> names2 = new ArrayList<String>(names);
+       System.out.println("Print names2 array: " + names2);
+       
+       
+       //I
+       names.remove(0);
+       System.out.println("Print names array: " + names);
+       System.out.println("Print names2 array: " + names2);
+
    }
 } 
